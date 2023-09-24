d76af86c45f1ff81b5c6a3e2739df1b63af31ea7
Antony Ramos <antony.ramos@radiofrance.com>
2023-09-24 15:30:10 +0200

    chore: create interfaces for usecases

--- internal/controller/discord/main.go
+++ internal/controller/discord/main.go
@@ -7,16 +7,14 @@ import (
 	"time"
 
 	"github.com/antony-ramos/guildops/internal/entity"
-
-	"github.com/antony-ramos/guildops/internal/usecase"
 )
 
 type Discord struct {
 	AbsenceUseCase
 	PlayerUseCase
-	*usecase.StrikeUseCase
-	*usecase.LootUseCase
-	*usecase.RaidUseCase
+	StrikeUseCase
+	LootUseCase
+	RaidUseCase
 }
 
 var ctxError = "Error because request took too much time to complete"
@@ -32,6 +30,26 @@ type PlayerUseCase interface {
 	DeletePlayer(ctx context.Context, playerName string) error
 	ReadPlayer(ctx context.Context, playerName string) (entity.Player, error)
 	LinkPlayer(ctx context.Context, playerName string, discordID string) error
+}
+
+type RaidUseCase interface {
+	CreateRaid(ctx context.Context, raidName, difficulty string, date time.Time) (entity.Raid, error)
+	DeleteRaid(ctx context.Context, raidID int) error
+}
+
+type StrikeUseCase interface {
+	CreateStrike(ctx context.Context, strikeReason, playerName string) error
+	DeleteStrike(ctx context.Context, id int) error
+	ReadStrikes(ctx context.Context, playerName string) ([]entity.Strike, error)
+}
+
+type LootUseCase interface {
+	CreateLoot(ctx context.Context, lootName string, raidID int, playerName string) error
+	ListLootOnPLayer(ctx context.Context, playerName string) ([]entity.Loot, error)
+	SelectPlayerToAssign(
+		ctx context.Context, playerNames []string, difficulty string,
+	) (entity.Player, error)
+	DeleteLoot(ctx context.Context, lootID int) error
 }
 
 // HumanReadableError returns the error message without the package name.
